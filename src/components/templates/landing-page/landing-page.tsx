eefc4eaf43bd14262576762e2a80fa9803563004
ryanx3 <ryangabriel_cap@hotmail.com>
2025-09-20 19:41:12 +0100

    refactor

---
+++ src/components/templates/landing-page/landing-page.tsx
@@ -0,0 +1,17 @@
+import { CallToAction } from "./sections/call-to-action"
+import { CustomerStorySection } from "./sections/customer-story-section"
+import { FeatureSection } from "./sections/feature-section"
+import { HeroSection } from "./sections/hero-section"
+import { SupportSection } from "./sections/support-section"
+
+export const LandingPage = () => {
+  return (
+    <article className="flex flex-col gap-10 md:gap-20">
+      <HeroSection />
+      <FeatureSection />
+      <SupportSection />
+      <CustomerStorySection />
+      <CallToAction />
+    </article>
+  )
+}
