b7cf08e1ced3ea87e62e4a930c1f961655166f55
ryanx3 <ryangabriel_cap@hotmail.com>
2025-09-23 17:17:41 +0100

    feat: creating blog page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { LandingPage } from "@/components/templates/landing-page";
-import { allPosts } from 'contentlayer/generated'
 export default function Home() {
-  console.log(allPosts)
   return (
     <LandingPage />
   );
