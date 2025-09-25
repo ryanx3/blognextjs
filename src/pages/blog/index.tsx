ae5b89e60c307e8ddbf4d0c803e938484ed2d91c
ryanx3 <ryangabriel_cap@hotmail.com>
2025-09-25 20:42:34 +0100

    feat: created Search component without states to search, just router from nextjs

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,10 +1,11 @@
+import { Search } from "@/components/search";
+
 export default function BlogPage() {
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
       <header className="">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
-            {/* TAG */}
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
               BLOG
             </span>
@@ -14,6 +15,7 @@ export default function BlogPage() {
               Dicas e estratégias para impulsionar seu negócio
             </h1>
           </div>
+      <Search/>
         </div>
       </header>
 
