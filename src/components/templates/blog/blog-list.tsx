bc8b22f41f237aa6e7c5b87deb238184b81fd941
ryanx3 <ryangabriel_cap@hotmail.com>
2025-09-30 13:30:07 +0100

    feat: created post-grid-card component

--- src/components/templates/blog/blog-list.tsx
+++ src/components/templates/blog/blog-list.tsx
@@ -1,6 +1,7 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
-import { PostCard } from "./components";
+import { PostCard } from "./components/post-card/post-card";
+import { PostGridCard } from "./components/post-grid";
 
 export const BlogList = () => {
   const router = useRouter()
@@ -8,7 +9,7 @@ export const BlogList = () => {
   const pageTitle = query ? `Resultados para a pesquisa: "${query}"` : "Dicas e estratégias para impulsionar seu negócio"
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
-      <header className="">
+      <header className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -22,17 +23,19 @@ export const BlogList = () => {
           <Search />
         </div>
       </header>
-      <PostCard
-        title="Transformando seu negócio em uma loja virtual"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
-        date="20/12/2024"
-        slug="transformando"
-        image="/assets/primeiro-post.png"
-        author={{
-          avatar: '/customer-01.png',
-          name: 'Aspen Dokidis',
-        }}
-      />
+      <PostGridCard>
+        <PostCard
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
+          date="20/12/2024"
+          slug="transformando"
+          image="/assets/primeiro-post.png"
+          author={{
+            avatar: '/customer-01.png',
+            name: 'Aspen Dokidis',
+          }}
+        />
+      </PostGridCard>
     </div>
   );
 }
