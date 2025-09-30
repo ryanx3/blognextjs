0a552a2172725e52c9f23b019695b711414f7d3c
ryanx3 <ryangabriel_cap@hotmail.com>
2025-09-30 13:24:17 +0100

    feat: getting post-card data dynamically

--- src/components/templates/blog/blog-list.tsx
+++ src/components/templates/blog/blog-list.tsx
@@ -22,7 +22,17 @@ export const BlogList = () => {
           <Search />
         </div>
       </header>
-      <PostCard />
+      <PostCard
+        title="Transformando seu negócio em uma loja virtual"
+        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online..."
+        date="20/12/2024"
+        slug="transformando"
+        image="/assets/primeiro-post.png"
+        author={{
+          avatar: '/customer-01.png',
+          name: 'Aspen Dokidis',
+        }}
+      />
     </div>
   );
 }
